a1682044626b846d79a1890fdac07959e156f27e
BouchetitAnfel <anfel.bouchetit@univ-constantine2.dz>
2026-05-06 18:39:23 +0100

    Bcakend ready 100 after modifying url

--- server.js
+++ server.js
@@ -4,8 +4,13 @@ const connectDB = require('./src/config/db');
 
 const PORT = process.env.PORT || 5000;
 
-connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server running `);
+connectDB()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error("Database connection failed:", err);
+        process.exit(1);
     });
-});
